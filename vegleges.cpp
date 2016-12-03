f5d4be6bc853242d56ab3010817230765f968829
Bence Skroka <skorka.bence@gmail.com>
2016-12-03 11:36:59 +0100

    Veglegesites

--- vegleges.cpp
+++ vegleges.cpp
@@ -2,7 +2,6 @@
 GitHub: http://github.com/SkorkaBence/ProgalapBeadando
 */
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -18,6 +17,15 @@ public:
     int minimumPontszam;
 };
 
+// beolvasó algoritmusok
+void integerBeolvasas(int &N);
+void integerBeolvasas_Min(int &N, int minimum);
+void integerBeolvasas_MinMax(int &N, int minimum, int maximum);
+
+// feladatmegoldás lépéseit tartalmazó algoritmusok
+void beolvasas(Verseny versenyek[], Tanulo tanulok[maxN], int &versenyekSzama, int &tanulokSzama);
+void eredmeny(const Verseny versenyek[], const Tanulo tanulok[maxN], int versenyekSzama, int tanulokSzama);
+
 int main()
 {
     // adatokat tároló változók
@@ -25,23 +33,82 @@ int main()
     int tanulokSzama;
     Verseny versenyek[maxN];
     Tanulo tanulok[maxN];
+
+    beolvasas(versenyek, tanulok, versenyekSzama, tanulokSzama);
+    eredmeny(versenyek, tanulok, versenyekSzama, tanulokSzama);
+
+    return 0;
+}
+
+void integerBeolvasas(int &N) {
+    bool hiba = false;
+    do {
+        cin >> N;
+        hiba = cin.fail();
+        if (hiba) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cerr << "Hibas bemenet, egesz szám szukseges!" << endl;
+        }
+    } while (hiba);
+}
+
+void integerBeolvasas_Min(int &N, int minimum) {
+    bool hiba = false;
+    do {
+        cin >> N;
+        hiba = cin.fail();
+        if (!hiba) {
+            if (N < minimum) {
+                hiba = true;
+            }
+        }
+        if (hiba) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cerr << "Hibas bemenet, egesz szam szukseges, amely minimum " << minimum << "!" << endl;
+        }
+    } while (hiba);
+}
+
+void integerBeolvasas_MinMax(int &N, int minimum, int maximum) {
+    bool hiba = false;
+    do {
+        cin >> N;
+        hiba = cin.fail();
+        if (!hiba) {
+            if ((N < minimum) || (N > maximum)) {
+                hiba = true;
+            }
+        }
+        if (hiba) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cerr << "Hibas bemenet, egesz szam szukseges, amely minimum " << minimum << " es maximum " << maximum << "!" << endl;
+        }
+    } while (hiba);
+}
+
+void beolvasas(Verseny versenyek[], Tanulo tanulok[maxN], int &versenyekSzama, int &tanulokSzama) {
     // egyéb változók
     int minimumPontszam;
     int indultTanulokSzama;
     int tanuloid;
     int pontszam;
-    int szamolas;
 
     // tanulók számának bekérdezése, és a változókat tároló tömb előkészítése
-    cin >> tanulokSzama;
+    cerr << "Tanulok szama: ";
+    integerBeolvasas_MinMax(tanulokSzama, 0, maxN);
     for (int i = 0; i < tanulokSzama; i++) {
         tanulok[i] = Tanulo();
     }
 
     // versenyek számának bekérdezése, és a versenyeket tároló tömb előkészítése, majd feltöltése a minimum pontszámokkal
-    cin >> versenyekSzama;
+    cerr << "Versenyek szama: ";
+    integerBeolvasas_MinMax(versenyekSzama, 0, maxN);
     for (int i = 0; i < versenyekSzama; i++) {
-        cin >> minimumPontszam;
+        cerr << (i+1) << ". verseny minimum pontszama: ";
+        integerBeolvasas_Min(minimumPontszam, 0);
         versenyek[i] = Verseny();
         versenyek[i].minimumPontszam = minimumPontszam;
     }
@@ -49,11 +116,14 @@ int main()
     // a versenyek adatainak bekérdezése
     for (int i = 0; i < versenyekSzama; i++) {
         // hányan indultak a versenyen
-        cin >> indultTanulokSzama;
+        cerr << "Hanyan indultak az " << (i+1) << ". versenyen? ";
+        integerBeolvasas_MinMax(indultTanulokSzama, 0, tanulokSzama);
         for (int j = 0; j < indultTanulokSzama; j++) {
             // a versenyen induló pontszámainak bekérdezése
-            cin >> tanuloid;
-            cin >> pontszam;
+            cerr << (i+1) << ". verseny " << (j+1) << ". indulojanak azonositoja: ";
+            integerBeolvasas_MinMax(tanuloid, 1, tanulokSzama + 1);
+            cerr << "Es pontszama: ";
+            integerBeolvasas_Min(pontszam, 0);
 
             if (pontszam >= versenyek[i].minimumPontszam) { // elérte a minimum pontszámot?
                 // sikerült neki!
@@ -61,8 +131,10 @@ int main()
             }
         }
     }
+}
 
-    szamolas = 0;
+void eredmeny(const Verseny versenyek[], const Tanulo tanulok[maxN], int versenyekSzama, int tanulokSzama) {
+    int szamolas = 0;
     // hány tanuló nem érte el a minimum pontot?
     for (int i = 0; i < tanulokSzama; i++) {
         if (!tanulok[i].sikerultEgyVerseny) {
@@ -71,12 +143,12 @@ int main()
     }
 
     // eredmény kiírása
+    cerr << endl << "Minimum pontszamot sehol el nem erok szama: ";
     cout << szamolas;
+    cerr << endl << "Es az azonositoik:";
     for (int i = 0; i < tanulokSzama; i++) {
         if (!tanulok[i].sikerultEgyVerseny) {
             cout << " " << i + 1;
         }
     }
-
-    return 0;
 }
